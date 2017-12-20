9cacd476d400b87fddccdaa6a9b746d9d4808482
Gabriel Cabral <gabriel.cabral@questmanager.com>
2017-12-20 19:27:23 -0200

    commit

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-(function (){
-var imagens = ['img/facebook.png','img/android.png','img/chrome.png','img/firefox.png','img/html5.png','img/googleplus.png','img/twitter.png','img/windows.png','img/cross.png'];
+// (function() {
+//   var imagens = [
+//     "img/facebook.png",
+//     "img/android.png",
+//     "img/chrome.png",
+//     "img/firefox.png",
+//     "img/html5.png",
+//     "img/googleplus.png",
+//     "img/twitter.png",
+//     "img/windows.png",
+//     "img/cross.png"
+//   ];
+// })();
 
+// app.inicio();
 
+$(document).ready(function() {
+  var imagens = [
+    "img/facebook.png",
+    "img/android.png",
+    "img/chrome.png",
+    "img/firefox.png",
+    "img/html5.png",
+    "img/googleplus.png",
+    "img/twitter.png",
+    "img/windows.png",
+    "img/cross.png"
+  ];
 
+  let cells = $("td");
 
-})();
+  for (cell in cells) {
+    let i = 0;
+    $('td').append("<img src=" + imagens[i] + ">");
+    i++;
+  }
+});
 
-app.inicio();
+$("button").on("click", function() {});
