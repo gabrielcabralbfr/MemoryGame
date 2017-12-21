9bf4ae1292f3a855a8d5849feb1f682c568ea1a8
GBNOTE\gabri <gabrielcabralbfr@gmail.com>
2017-12-21 20:39:06 -0200

    Final commit

--- script.js
+++ script.js
@@ -1,39 +1,134 @@
-// (function() {
-//   var imagens = [
-//     "img/facebook.png",
-//     "img/android.png",
-//     "img/chrome.png",
-//     "img/firefox.png",
-//     "img/html5.png",
-//     "img/googleplus.png",
-//     "img/twitter.png",
-//     "img/windows.png",
-//     "img/cross.png"
-//   ];
-// })();
-
-// app.inicio();
-
-$(document).ready(function() {
-  var imagens = [
-    "img/facebook.png",
-    "img/android.png",
-    "img/chrome.png",
-    "img/firefox.png",
-    "img/html5.png",
-    "img/googleplus.png",
-    "img/twitter.png",
-    "img/windows.png",
-    "img/cross.png"
+(function MemoryGame() {
+  const imagens = ["img/facebook.png", "img/facebook.png", "img/android.png", "img/android.png", "img/chrome.png", "img/chrome.png",
+    "img/firefox.png", "img/firefox.png", "img/html5.png", "img/html5.png", "img/googleplus.png", "img/googleplus.png",
+    "img/twitter.png", "img/twitter.png", "img/windows.png", "img/windows.png"
   ];
 
-  let cells = $("td");
+  const crossImage = "img/cross.png";
+  const board = document.getElementById('tabuleiro');
 
-  for (cell in cells) {
-    let i = 0;
-    $('td').append("<img src=" + imagens[i] + ">");
-    i++;
+  let count = 0;
+  let option1 = "";
+  let option2 = "";
+
+  let initialTime = 0;
+  let finalTime = 0;
+  let timeElapsed = 0;
+
+  function randomizeImages() {
+    Array.prototype.randomize = function () {
+      let i = this.length;
+      let j;
+      let temp;
+      while (--i) {
+        j = Math.floor(Math.random() * (i - 1));
+        temp = this[i];
+        this[i] = this[j];
+        this[j] = temp;
+      }
+    };
+    imagens.randomize();
   }
-});
 
-$("button").on("click", function() {});
+  function showCards() {
+    let html = "<ul>";
+    let j = 0;
+    for (let i = 0; i < 16; i++) {
+      html += "<li>";
+      html += "<img class='card front' src ='" + imagens[i] + "' />";
+      html += "<img class='cross back' src ='" + crossImage + "' />";
+      html += "</li>";
+    }
+    html += "</ul>";
+
+    board.innerHTML = "<button id='iniciar-jogo'>Iniciar Jogo</button>";
+    board.innerHTML += html;
+  }
+
+  function turnCardUp(card) {
+    card.addClass("face-up");
+    card.children(".card").show();
+    card.children(".cross").hide();
+  }
+
+  function turnCardsDown() {
+    $("li.face-up:not(.match)").removeClass("face-up");
+    card1 = "";
+    card2 = "";
+
+    setTimeout(function () {
+      $("li:not(.match)").children(".cross").show();
+      $("li:not(.match)").children(".card").hide();
+    }, 1500)
+  }
+
+  function checkCardParity(card1, card2) {
+    if (card1 === card2) {
+      $(this).addClass("match");
+      $('img[src="' + card1 + '"]').parent().addClass("match");
+      count = 0;
+    } else {
+      turnCardsDown();
+      setTimeout(function () {
+        count = 0;
+      }, 1500);
+    }
+  }
+
+  function gameEnded() {
+    if ($("li:not(.match)").length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function endGame() {
+    finalTime = new Date();
+    timeElapsed = finalTime - initialTime;
+    Math.round(timeElapsed /= 1000);
+    alert(`Você venceu! \n Sua partida durou: ${timeElapsed} segundos \n Iniciando um novo jogo..`);
+    initGame();
+  }
+
+  function initGame() {
+    initialTime = new Date();
+    alert("Você tem 3 segundos para memorizar o jogo");
+    randomizeImages();
+    showCards();
+
+    setTimeout(function () {
+      $(".card").hide();
+      $(".cross").show();
+    }, 3000);
+
+    $(document).on('click', 'li', function () {
+      if ((count < 2) && ($(this).hasClass("face-up")) === false) {
+        count++;
+        turnCardUp($(this))
+
+        switch (count) {
+          case 1:
+            option1 = $(this).children('.card').attr('src');
+            break;
+          case 2:
+            option2 = $(this).children('.card').attr('src');
+            checkCardParity(option1, option2)
+            break;
+        }
+      }
+
+      if (gameEnded()) {
+        endGame();
+      }
+    })
+  }
+
+  $(document).ready(function () {
+    showCards();
+
+    $(document).on('click', '#iniciar-jogo', function () {
+      initGame();
+    });
+  });
+})()
